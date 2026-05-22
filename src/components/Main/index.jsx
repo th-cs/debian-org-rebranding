cfb51d71aa62712e4a64a4751e66ab306aafd60a
th-cs <202718515+th-cs@users.noreply.github.com>
2026-05-22 02:04:24 -0300

    style: Change indentation

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,14 +1,15 @@
 import "./style.scss";
-import Hero from "../Hero/index.jsx";
+import Hero from "../Hero";
 
 function Main() {
 	return (
 		<main>
 			<Hero
 				heading="O Sistema Operacional Universal."
-				subheading="Livre para qualquer pessoa usar, modificar e distribuir"
-				subtitle="*Fotografias capturadas da Estação Espacial Internacional
-				da NASA, onde os laptops possuem Debian."/>
+				subheading="Livre para qualquer pessoa usar, modificar e
+					distribuir"
+				subtitle="*Fotografias capturadas da Estação Espacial
+					Internacional da NASA, onde os laptops possuem Debian."/>
 		</main>
 	);
 }
